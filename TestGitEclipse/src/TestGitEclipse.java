fedb651f45bb563736afe3d85483a0dc9152dd3b
Miguel Garcia <miguelvisgarcia@gmail.com>
2015-09-02 11:03:05 +0200

    This is the initial version.

---
+++ TestGitEclipse/src/TestGitEclipse.java
@@ -0,0 +1,26 @@
+/**
+ * TestGitEclipse.java
+ */
+
+/**
+ * Just a test for GIT & Eclipse.
+ * 
+ * @version 1.00
+ * @since 02 Sep 2015
+ * @author Miguel
+ *
+ */
+public class TestGitEclipse {
+
+	/**
+	 * Run the program.
+	 * 
+	 * @param args  command arguments
+	 */
+	public static void main(String[] args) {
+		
+		// Print some text
+		System.out.println("Hey!\n\nThis is just a test for GIT & Eclipse!");
+	}
+
+}
